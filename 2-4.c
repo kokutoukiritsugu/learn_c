100a3d7c4166b08700f2f9fc8114569d0a94d8e2
kokutoukiritsugu <kokutoukiritsugu@gmail.com>
2017-03-17 15:59:47 +0800

    get args , change to one line.

--- 2-4.c
+++ 2-4.c
@@ -2,7 +2,7 @@
 
 void squeeze(char[], char[]);
 
-unsigned int any(char[], char[]);
+int any(char[], char[]);
 
 int main() {
     char c1[] = "sdf1asdf1asdfa";
@@ -29,7 +29,7 @@ void squeeze(char s1[], char s2[]) {
     s1[k] = '\0';
 }
 
-unsigned int any(char s1[], char s2[]) {
+int any(char s1[], char s2[]) {
     for (int i = 0; s1[i] != '\0'; i++) {
         for (int j = 0; s2[j] != '\0'; j++) {
             if (s1[i] == s2[j]) {
